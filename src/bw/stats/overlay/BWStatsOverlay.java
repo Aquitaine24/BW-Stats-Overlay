6045c98888661b228af803151e98044b25c47b73
azmaan-din <njt1291@autuni.ac.nz>
2024-06-11 02:41:05 +1200

    Testing Gui

--- src/bw/stats/overlay/BWStatsOverlay.java
+++ src/bw/stats/overlay/BWStatsOverlay.java
@@ -4,28 +4,49 @@
  */
 package bw.stats.overlay;
 
+import java.awt.EventQueue;
 import java.io.IOException;
-import java.util.List;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.formdev.flatlaf.FlatDarkLaf;
 
 /**
  *
  * @author azmaa
  */
-public class BWStatsOverlay {
+public class BWStatsOverlay{
 
     /**
      * @param args the command line arguments
      * @throws IOException 
      */
     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
-        List<String> players = GetPlayers.GetPlayers();
 
-        if(players !=null){
-            for (String name : players) {
-                GetPlayerData.GetPlayerData(name);
-            }
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
         }
+
+        EventQueue.invokeLater(() -> {
+            try {
+                BedWarsStatsPanel frame = new BedWarsStatsPanel();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+
+        // List<String> players = GetPlayers.GetPlayers();
+
+        // if(players !=null){
+        //     for (String name : players) {
+        //         GetPlayerData.GetPlayerData(name);
+        //     }
+        // }
 
 
 
@@ -34,5 +55,6 @@ public class BWStatsOverlay {
         // //GetPlayerData.GetPlayerData(players.get(2));
         // //GetPlayerData.GetPlayerData("WildPlaysGame");
     }
+
     
 }
---
+++ src/bw/stats/overlay/BedWarsStatsPanel.java
@@ -0,0 +1,13 @@
+package bw.stats.overlay;
+
+import javax.swing.JFrame;
+
+public class BedWarsStatsPanel extends JFrame {
+    public BedWarsStatsPanel(){
+        setTitle("Bed Wars Stats");
+        setBounds(100, 100, 570, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+
+}
